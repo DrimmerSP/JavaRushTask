2ad6368c4b99cb508fe1e8ef21814b18645b0653
Drimmer <drimmer88@gmail.com>
2021-08-14 15:26:20 +0300

    +2 tasks

--- 1.JavaSyntax/src/com/javarush/task/task05/task0513/Rectangle.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0513/Rectangle.java
@@ -6,7 +6,29 @@ package com.javarush.task.task05.task0513;
 
 public class Rectangle {
     //напишите тут ваш код
+    int top;
+    int left;
+    int width;
+    int height;
 
+    public void initialize(int top){
+        this.top = top;
+    }
+    public void initialize(int top, int left){
+        this.top = top;
+        this.left = left;
+    }
+    public void initialize(int top, int left, int width){
+        this.top = top;
+        this.left = left;
+        this.width = width;
+    }
+    public void initialize(int top, int left, int width, int height){
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+    }
     public static void main(String[] args) {
 
     }
---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0514/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task05.task0514;
+
+/* 
+Программист создает человека
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Person person = new Person();
+        person.initialize("Bob", 24);
+    }
+
+    static class Person {
+        //напишите тут ваш код
+        String name;
+        int age;
+        public void initialize(String name, int age){
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
