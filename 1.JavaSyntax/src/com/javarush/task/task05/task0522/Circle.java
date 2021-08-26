c1c615951017e23283fec07f709f20691f15f84d
Sergei Pliuta <drimmer88@gmail.com>
2021-08-26 22:39:02 +0300

    lecture 09 done

--- 1.JavaSyntax/src/com/javarush/task/task05/task0522/Circle.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0522/Circle.java
@@ -10,6 +10,22 @@ public class Circle {
     public double radius;
 
     //напишите тут ваш код
+    public Circle(){}
+    public Circle(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+    public Circle(double x, double y){
+        this.x = x;
+        this.y = y;
+        this.radius = 15;
+    }
+    public Circle(double x){
+        this.x = x;
+        this.y = 2;
+        this.radius = 14;
+    }
 
     public static void main(String[] args) {
 
